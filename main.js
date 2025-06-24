a769ce8ee36385156025f55f4f114e94b12a4375
CRSAds <yuri@crsadvertising.com>
2025-06-24 13:43:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+// main.js
 import initFlow from './initFlow.js';
-import { setupFormSubmit } from './formSubmit.js'; // ✅ goed
+import { setupFormSubmit } from './formSubmit.js';
 import { setupImageFix } from './imageFix.js';
+import { handleFooterDisplay } from './footerControl.js';
 
 setupImageFix();
+handleFooterDisplay();
 initFlow();
 setupFormSubmit();
