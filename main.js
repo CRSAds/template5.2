3ac347f81ed2cd0256e67d07d391d1b4208a52b9
CRSAds <yuri@crsadvertising.com>
2025-06-24 15:59:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // main.js
+import { handleFooterDisplay } from './footerControl.js';
 import initFlow from './initFlow.js';
 import { setupFormSubmit } from './formSubmit.js';
 import { setupImageFix } from './imageFix.js';
-import { handleFooterDisplay } from './footerControl.js';
 
 setupImageFix();
 handleFooterDisplay();
