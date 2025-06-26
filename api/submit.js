30d76c17dcb2c2d4bd719f12eb64e34b3f254aca
CRSAds <yuri@crsadvertising.com>
2025-06-26 09:25:23 +0200

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -5,8 +5,8 @@ let recentIps = new Map();
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cache-Control');
+  
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   if (req.method !== 'POST') {
