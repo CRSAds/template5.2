1ae38e1455da73216dd7621e8850848e131977e2
CRSAds <yuri@crsadvertising.com>
2025-06-27 13:25:16 +0200

    Create sponsorpopup.js

---
+++ sponsorpopup.js
@@ -0,0 +1,32 @@
+const sponsorData = {
+  "intro": {
+    "title": "Sponsors",
+    "description": "Hieronder vindt u de sponsors van deze campagne welke u graag hun allerbeste deals en aanbiedingen willen laten zien. U kunt per sponsor aangeven of zij uw data mogen ontvangen, vink simpelweg het vakje naast of onder hun naam aan. Per sponsor is duidelijk aangegeven waarvoor zij contact met u willen opnemen, hoe vaak zij contact met u willen opnemen en via welke manier u gecontacteerd gaat worden (telefoon of email). Tevens staat er bij elke sponsor een link naar hun privacy voorwaarden."
+  },
+  "telemarketing": [
+    {
+      "name": "My Collections",
+      "description": "My Collections biedt u vele voordelen: naast de voordelige kennismakingszending zijn ook de overige producten altijd zeer laag geprijsd! Daarnaast worden alle zendingen gemakkelijk bij u thuisbezorgd. My Collections biedt veel en vraagt weinig.",
+      "logo": "",
+      "address": "My Collections BV\nOosteinde 137\n2271 EE Voorburg\nNederland",
+      "privacy": "Privacy Policy",
+      "checkboxLabel": "MyCollections mag mij bellen met een eenmalig aanbod"
+    },
+    {
+      "name": "Raadselgids",
+      "description": "Wij brengen elke dag verschillende exclusieve en vooral uitdagende games uit op onze platform die enkel voor onze leden gelden. U kunt dagelijks woordspellen of andere games spelen en verschillende prijzen verdienen.",
+      "logo": "",
+      "address": "Premium Services B.V\nBredaseweg 185\n4872LA, Etten-Leur\nNederland",
+      "privacy": "Privacy Policy",
+      "checkboxLabel": "Raadselgids mag mij bellen met een eenmalig aanbod"
+    },
+    {
+      "name": "Consubeheer",
+      "description": "Bespaar met onze energie specialisten. Consubeheer biedt u de beste energiedeals aan van dit moment.",
+      "logo": "",
+      "address": "Jan Rebelstraat 14A\n1069CC Amsterdam\nNederland",
+      "privacy": "Privacy Policy",
+      "checkboxLabel": "Consubeheer mag mij bellen met een eenmalig aanbod"
+    }
+  ]
+};
