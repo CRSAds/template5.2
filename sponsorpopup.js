079f16768e6fcd42ad90a14183a95047af3ad471
CRSAds <yuri@crsadvertising.com>
2025-07-09 11:11:32 +0200

    Update sponsorpopup.js

--- sponsorpopup.js
+++ sponsorpopup.js
@@ -3,7 +3,7 @@ const imageBaseUrl = "https://template5-2.vercel.app/assets/sponsorlogo/";
 window.renderSponsorPopup = function (container) {
   const sponsorData = {
     heading: "Sponsors",
-    intro: `Hieronder vindt u de sponsors van deze campagne welke u graag hun allerbeste deals en aanbiedingen willen laten zien. U kunt per sponsor aangeven of zij uw data mogen ontvangen, vink simpelweg het vakje naast of onder hun naam aan. Per sponsor is duidelijk aangegeven waarvoor zij contact met u willen opnemen, hoe vaak zij contact met u willen opnemen en via welke manier u gecontacteerd gaat worden (telefoon of email). Tevens staat er bij elke sponsor een link naar hun privacy voorwaarden. Wij raden aan dat u deze privacy voorwaarden doorneemt.`,
+    intro: `Hieronder vindt u de sponsors van deze campagne welke u graag hun allerbeste deals en aanbiedingen willen laten zien. Per sponsor is duidelijk aangegeven waarvoor zij contact met u willen opnemen, hoe vaak zij contact met u willen opnemen en via welke manier u gecontacteerd gaat worden (telefoon of email). Tevens staat er bij elke sponsor een link naar hun privacy voorwaarden. Wij raden aan dat u deze privacy voorwaarden doorneemt.`,
     sections: [
       {
         title: "\ud83d\udcde Telemarketing sponsors",
