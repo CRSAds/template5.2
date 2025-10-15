6e9147244566e3cc17801fe62b164d44bd09ee58
CRSAds <yuri@crsadvertising.com>
2025-10-15 09:12:31 +0200

    Update sponsorCampaigns.js

--- sponsorCampaigns.js
+++ sponsorCampaigns.js
@@ -114,11 +114,11 @@ const sponsorCampaigns = {
     hasCoregFlow: true,
     answerFieldKey: "f_2575_coreg_answer_dropdown"
   },
-    "campaign-groenen": {
+    "campaign-groenevrienden": {
     cid: 5446,
     sid: 34,
     requiresLongForm: true,
-    coregAnswerKey: "coreg_answer_campaign-groenen",
+    coregAnswerKey: "coreg_answer_campaign-groenevrienden",
     hasCoregFlow: false
   },
     "campaign-vastelasten": {
